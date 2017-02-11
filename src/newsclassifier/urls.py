f6b9cc28f3b3f4f5e8df619b01d784bad141edf8
Taranjeet Singh <reachtotj@gmail.com>
2017-02-11 12:43:28 +0530

    Update static files, add templates.

--- src/newsclassifier/urls.py
+++ src/newsclassifier/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^admin_tools/', include('admin_tools.urls')),
+
+    url(r'^test/$', TemplateView.as_view(template_name='base.html')),
 ]
